a99aa73ac3f99a6136b0c2f932f721e65729b729
ExcessByte <ericdenny26@gmail.com>
2026-09-20 21:21:45 +0530

    Adding testing functionality

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	args := os.Args[1:]
+	if len(args) == 0 {
+		fmt.Println("Please provide a url")
+		return
+	}
+
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+	}
+
+	for _, url := range args {
+		checkStatus(client, url)
+	}
+}
+
+func checkStatus(client *http.Client, url string) {
+	resp, err := client.Get(url)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	fmt.Println(url, ": ", resp.Status)
 }
